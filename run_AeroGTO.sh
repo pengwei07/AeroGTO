750ee269d122590d2d9e1ebea2742382b148976b
LiuPengwei <142504218+pengwei07@users.noreply.github.com>
2024-08-20 14:54:58 +0800

    Update run_AeroGTO.sh

--- run_AeroGTO.sh
+++ run_AeroGTO.sh
@@ -1,5 +1,3 @@
-# sudo iptables -A INPUT -p tcp --dport 5928 -j ACCEPT
-
 export OMP_NUM_THREADS=32
 export NCCL_P2P_DISABLE=1 
 export TORCH_DISTRIBUTED_DEBUG=INFO
@@ -7,5 +5,22 @@ export TORCH_DISTRIBUTED_DEBUG=DETAIL
 export CUDA_LAUNCH_BLOCKING=1
 
 export CUDA_VISIBLE_DEVICES=0,1,2
+# ./configs/$case_name: stores the corresponding config files of a case
 
-python main.py --config ./configs/AeroGTO_4x.json
+# 1. AeroGTO
+python main.py --config ./configs/AeroGTO.json
+
+# 2. GNOT
+python main.py --config ./configs/GNOT.json
+
+# 3. IPOT
+python main.py --config ./configs/IPOT.json
+
+# 4. MGN
+python main.py --config ./configs/MGN.json
+
+# 5. Transolver
+python main.py --config ./configs/Transolver.json
+
+# 6. GINO
+# see ./gino/
